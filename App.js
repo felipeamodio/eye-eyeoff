a168b771bc018c1dbacc1af7a505614ead2c9024
Felipe <fealves.amodio@gmail.com>
2021-03-30 19:15:02 -0300

    hidepass

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
 export default function App() {
+
+  const [input, setInput] = useState('');
+  const [hidePass, setHidePass] = useState(true); {/**passando o ! de negaçao lá no onPress ele pega o estado hidePass q estava true e passa pra falso */}
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputArea}>
+        <TextInput
+          style={styles.input}
+          placeholder="Insira sua senha"
+          placeholderTextColor="#FFFFFF"
+          value={input}
+          onChangeText={(texto) => setInput(texto)}
+          secureTextEntry={hidePass}
+        />
+        <TouchableOpacity style={styles.icon} onPress={() => setHidePass(!hidePass)}> 
+        {hidePass 
+          ?
+          <Ionicons name="eye" color="#FFFFFF" size={25} />
+          :
+          <Ionicons name="eye-off" color="#FFFFFF" size={25} />
+      }
+          
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -18,4 +40,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputArea: {
+    flexDirection: 'row',
+    width: '90%',
+    backgroundColor: '#121212',
+    borderRadius: 5,
+    height: 50,
+    alignItems: 'center'
+  },
+  input: {
+    width: '85%',
+    height: 50,
+    color: '#FFFFFF',
+    padding: 8,
+    fontSize: 18
+  },
+  icon: {
+    width: '15%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
